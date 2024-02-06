d650e2fa35e759174f83b97107ba03e548565d3d
Anne Heinrich <67550502+annehxx@users.noreply.github.com>
2024-02-06 11:14:04 +0100

    add icons

--- app.js
+++ app.js
@@ -24,4 +24,13 @@ function openMenu() {
   sideMenu.style.right = "0";
 }
 
+const scriptURL =
+"https://script.google.com/macros/s/AKfycbxBK1ETokBqjBjK019uoaDNxz-Bk98vr9BkZ8xba6p2Y0hxubcekveaG58W_IzwwA6C/exec";
+const form = document.forms["submit-to-google-sheet"];
 
+form.addEventListener("submit", (e) => {
+e.preventDefault();
+fetch(scriptURL, { method: "POST", body: new FormData(form) })
+  .then((response) => console.log("Success!", response))
+  .catch((error) => console.error("Error!", error.message));
+});
