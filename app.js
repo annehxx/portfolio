a8b8086a3b0f4cba11770548a7d170d5e297f391
Anne Heinrich <67550502+annehxx@users.noreply.github.com>
2024-02-10 15:44:37 +0100

    Show success message when successfully sent

--- app.js
+++ app.js
@@ -17,20 +17,38 @@ function openTab(tabName) {
 var sideMenu = document.getElementById("sidemenu");
 
 function closeMenu() {
-    sideMenu.style.right = "-200px";
+  sideMenu.style.right = "-200px";
 }
 
 function openMenu() {
   sideMenu.style.right = "0";
 }
 
+//-------------Submit Form---------------
+
 const scriptURL =
-"https://script.google.com/macros/s/AKfycbxBK1ETokBqjBjK019uoaDNxz-Bk98vr9BkZ8xba6p2Y0hxubcekveaG58W_IzwwA6C/exec";
+  "https://script.google.com/macros/s/AKfycbxBK1ETokBqjBjK019uoaDNxz-Bk98vr9BkZ8xba6p2Y0hxubcekveaG58W_IzwwA6C/exec";
 const form = document.forms["submit-to-google-sheet"];
 
-form.addEventListener("submit", (e) => {
-e.preventDefault();
-fetch(scriptURL, { method: "POST", body: new FormData(form) })
-  .then((response) => console.log("Success!", response))
-  .catch((error) => console.error("Error!", error.message));
+const successMessage = document.getElementById("success-message");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  successMessage.style.display = "block";
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    fetch(scriptURL, { method: "POST", body: new FormData(form) })
+      .then((response) => console.log("Success!", response))
+      .catch((error) => console.error("Error!", error.message));
+  });
+
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("message").value = "";
+
+  setTimeout(function () {
+    successMessage.style.display = "none";
+  }, 5000);
 });
